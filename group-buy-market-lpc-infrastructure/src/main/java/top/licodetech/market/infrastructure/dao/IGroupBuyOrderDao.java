5e91a34a0489ac554cfeae90ab87cdfcd0cb856d
Li Peicheng <347647096@qq.com>
2026-03-29 20:41:04 +0800

    feat: tag-v1.0

--- group-buy-market-lpc-infrastructure/src/main/java/top/licodetech/market/infrastructure/dao/IGroupBuyOrderDao.java
+++ group-buy-market-lpc-infrastructure/src/main/java/top/licodetech/market/infrastructure/dao/IGroupBuyOrderDao.java
@@ -31,10 +31,10 @@ public interface IGroupBuyOrderDao {
 
     List<GroupBuyOrder> queryGroupBuyProgressByTeamIds(@Param("teamIds") Set<String> teamIds);
 
-    Integer queryAllTeamCount(Set<String> teamIds);
+    Integer queryAllTeamCount(@Param("teamIds") Set<String> teamIds);
 
-    Integer queryAllTeamCompleteCount(Set<String> teamIds);
+    Integer queryAllTeamCompleteCount(@Param("teamIds") Set<String> teamIds);
 
-    Integer queryAllUserCount(Set<String> teamIds);
+    Integer queryAllUserCount(@Param("teamIds") Set<String> teamIds);
 
 }
