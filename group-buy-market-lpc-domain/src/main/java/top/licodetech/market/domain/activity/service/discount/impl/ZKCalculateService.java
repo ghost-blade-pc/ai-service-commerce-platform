224a6aed70e5376e212990367aa28bb6992ffa89
Li Peicheng <347647096@qq.com>
2026-03-30 21:56:41 +0800

    feat: tag-v2.0

--- group-buy-market-lpc-domain/src/main/java/top/licodetech/market/domain/activity/service/discount/impl/ZKCalculateService.java
+++ group-buy-market-lpc-domain/src/main/java/top/licodetech/market/domain/activity/service/discount/impl/ZKCalculateService.java
@@ -4,9 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import top.licodetech.market.domain.activity.model.valobj.GroupBuyActivityDiscountVO;
 import top.licodetech.market.domain.activity.service.discount.AbstractDiscountCalculateService;
-import top.licodetech.market.types.common.Constants;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Slf4j
 @Service("ZK")
@@ -19,7 +19,7 @@ public class ZKCalculateService extends AbstractDiscountCalculateService {
         String marketExpr = groupBuyDiscount.getMarketExpr();
 
         // 折扣价格
-        BigDecimal deductionPrice = originalPrice.multiply(new BigDecimal(marketExpr.trim()));
+        BigDecimal deductionPrice = originalPrice.multiply(new BigDecimal(marketExpr)).setScale(0, RoundingMode.DOWN);
 
 //        // 判断折扣后金额，最低支付1分钱
 //        if (deductionPrice.compareTo(BigDecimal.ZERO) <= 0) {
