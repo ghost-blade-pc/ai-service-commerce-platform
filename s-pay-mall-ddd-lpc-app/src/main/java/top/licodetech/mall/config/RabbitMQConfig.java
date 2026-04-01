a042d09af1e6dc5d20ca4d7a8be762cedee1c9b6
Li Peicheng <347647096@qq.com>
2026-04-01 17:01:48 +0800

    feat: 引入RabbitMQ分布式多端消费

---
+++ s-pay-mall-ddd-lpc-app/src/main/java/top/licodetech/mall/config/RabbitMQConfig.java
@@ -0,0 +1,36 @@
+package top.licodetech.mall.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author LiPC
+ * @description
+ * @create 2026-04-01 16:41
+ */
+@Configuration
+public class RabbitMQConfig {
+
+    /**
+     * 消费拼团消息
+     */
+    @Bean
+    public Binding topicTeamSuccessBinding(
+            @Value("${spring.rabbitmq.config.consumer.topic_team_success.exchange}") String exchangeName,
+            @Value("${spring.rabbitmq.config.consumer.topic_team_success.routing_key}") String routingKey,
+            @Value("${spring.rabbitmq.config.consumer.topic_team_success.queue}") String queue) {
+
+        // 消息生产方的交换机
+        TopicExchange topicExchange = new TopicExchange(exchangeName, true, false);
+
+        return BindingBuilder.bind(new Queue(queue, true))
+                .to(topicExchange)
+                .with(routingKey);
+    }
+
+}
---
+++ s-pay-mall-ddd-lpc-trigger/src/main/java/top/licodetech/mall/trigger/listener/TeamSuccessTopicListener.java
@@ -0,0 +1,34 @@
+package top.licodetech.mall.trigger.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author LiPC
+ * @description
+ * @create 2026-04-01 16:47
+ */
+@Slf4j
+@Component
+public class TeamSuccessTopicListener {
+
+    /**
+     * 指定消费队列
+     */
+    @RabbitListener(
+            bindings = @QueueBinding(
+                    value = @Queue(value = "${spring.rabbitmq.config.consumer.topic_team_success.queue}"),
+                    exchange = @Exchange(value = "${spring.rabbitmq.config.consumer.topic_team_success.exchange}", type = ExchangeTypes.TOPIC),
+                    key = "${spring.rabbitmq.config.consumer.topic_team_success.routing_key}"
+            )
+    )
+    public void listener(String message) {
+        log.info("接收消息:{}", message);
+    }
+
+}
